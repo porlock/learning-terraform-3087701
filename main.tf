d3a7ef58ba8d0cbb179d79b00475626700f68208
Tomasz Zieliński <porlock@porlock.waw.pl>
2023-01-17 17:51:45 +0100

    add publi subnets

--- main.tf
+++ main.tf
@@ -17,10 +17,11 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs  = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
  
   enable_vpn_gateway = true
 
