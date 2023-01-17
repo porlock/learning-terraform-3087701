e94afd4f8cc0e1362b0c78b42690370f6a81c11e
Tomasz Zieliński <porlock@porlock.waw.pl>
2023-01-17 18:21:28 +0100

    fix

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   
   tags = {
@@ -49,7 +50,7 @@ module "blog_sg" {
   version = "4.17.1"
   name    = "blog_new"
 
-  vpc_id = module.blog_vpc.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
